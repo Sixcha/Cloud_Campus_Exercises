c9bf1d78b24ff511f78097de0e5b0bbc91d887a8
Paul Bora <paulbora_akcan@hotmail.com>
2026-05-26 11:53:00 +0200

    Exercise 2-3

---
+++ Cloud_Campus_JS/Jour_1/2/script.js
@@ -0,0 +1,6 @@
+const nickname = "Paul"
+let score = 0
+
+// Le nom du joueur ne changera pas, mais le score peut changer, donc l'utilisation de const pour le nom et let pour le score
+
+console.log( "Le joueur" + nickname + "commence avec un score de " + score)
---
+++ Cloud_Campus_JS/Jour_1/3/script.js
@@ -0,0 +1,7 @@
+let stringVar = "duck"
+let numberVar = 9999
+let booleanVar = true
+
+console.log(Object.keys({stringVar})[0] + " est de type " + typeof stringVar)
+console.log(Object.keys({numberVar})[0] + " est de type " + typeof numberVar)
+console.log(Object.keys({booleanVar})[0] + " est de type " + typeof booleanVar)
